537edd381f7bd036e915ef8c63262fe1b3ad4577
ranjangreddy <ranjan.g.reddy@gmail.com>
2025-04-21 01:05:16 -0400

    checking the api redirection

--- client/App.tsx
+++ client/App.tsx
@@ -44,7 +44,7 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
 
   const login = async (email: string, password: string) => {
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
+      const res = await fetch(`https://final-project-part-2-algorithm-unlocked.onrender.com/api/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
